e90d4d8295cd42c8f46764af623ea5d9f97d2942
Nabor Torres <arsentau@gmail.com>
2023-01-18 15:28:15 -0300

    Add Decorator example

--- cmd/real_application_examples/main.go
+++ cmd/real_application_examples/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	servicesExample "github.com/Arsentau/golang-design-patterns/internal/pkg/bridge_services_example"
+	deco "github.com/Arsentau/golang-design-patterns/internal/pkg/decorator"
 )
 
 func main() {
 	servicesExample.Bridge()
+	deco.Decorator()
 }
--- internal/pkg/decorator/decorator.go
+++ internal/pkg/decorator/decorator.go
@@ -1,7 +1,35 @@
 package decorator
 
-import "fmt"
+import (
+	"log"
+)
 
+type IService interface {
+	CheckStatus() string
+}
+
+type Ns1Status struct{}
+
+func (l *Ns1Status) CheckStatus() string {
+	statusMessage := "Checking NS1 Server status"
+	return statusMessage
+}
+
+// Decorator
+type Logger struct {
+	service IService
+}
+
+func (l *Logger) LogStatus() {
+	statusMessage := l.service.CheckStatus()
+	log.Println("From Decorator:", statusMessage)
+}
+
+// Decorator implementation
 func Decorator() {
-	fmt.Println("Decorator")
+	ns1Service := &Ns1Status{}
+	logger := &Logger{
+		service: ns1Service,
+	}
+	logger.LogStatus()
 }
