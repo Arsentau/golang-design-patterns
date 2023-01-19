b825e960c662182980544fd1f7e841350ec73b54
Nabor Torres <arsentau@gmail.com>
2023-01-19 09:54:39 -0300

    Add better decorator example

--- internal/pkg/structural/decorator_example/decorator.go
+++ internal/pkg/structural/decorator_example/decorator.go
@@ -1,8 +1,44 @@
 package decorator
 
 import (
+	"errors"
 	"log"
 )
+
+func decorator(f func(s string) (string, error)) func(string) {
+	return func(s string) {
+		log.Println("Checking service status...")
+		msg, err := f(s)
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+		log.Println("After:", msg)
+	}
+}
+
+func CheckBadStatus(s string) (string, error) {
+	log.Println(s)
+	return "", errors.New("Could not connect to the server")
+}
+
+func CheckOkStatus(s string) (string, error) {
+	log.Println(s)
+	return "Online", nil
+}
+
+func Decorator() {
+
+	// without decorator
+	CheckBadStatus("NS1")
+
+	// with decorator
+	decorator(CheckBadStatus)("Route83")
+	decorator(CheckOkStatus)("Cloudflare")
+
+}
+
+// This is a second way to do it, but I consider that this way introduces more complexity and is more difficult to maintain.
 
 type IService interface {
 	CheckStatus() string
@@ -26,7 +62,8 @@ func (l *Logger) LogStatus() {
 }
 
 // Decorator implementation
-func Decorator() {
+func Decorator2() {
+
 	ns1Service := &Ns1Status{}
 	logger := &Logger{
 		service: ns1Service,
